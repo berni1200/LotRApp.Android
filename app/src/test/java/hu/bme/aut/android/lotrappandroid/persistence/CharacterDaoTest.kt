fc31b7fefba2d030a6e305cec39779295e6cd694
Papp Bernadett Judit <berni.bf@gmail.com>
2022-05-07 11:50:27 +0200

    test perzistence

--- app/src/test/java/hu/bme/aut/android/lotrappandroid/persistence/CharacterDaoTest.kt
+++ app/src/test/java/hu/bme/aut/android/lotrappandroid/persistence/CharacterDaoTest.kt
@@ -2,6 +2,7 @@ package hu.bme.aut.android.lotrappandroid.persistence
 
 import hu.bme.aut.android.lotrappandroid.model.LotRCharacter
 import hu.bme.aut.android.lotrappandroid.persistence.MockTestUtil.mockCharacterList
+import hu.bme.aut.android.lotrappandroid.persistence.MockTestUtil.testCharacterList
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
@@ -24,15 +25,30 @@ class CharacterDaoTest : LocalDatabase() {
     }
 
     @Test
-    fun loadCharacterListTest() = runBlocking{
-        val mockDataList = mockCharacterList()
+    fun characterListTest() = runBlocking{
+
+        //insert list into db
+        val mockDataList = testCharacterList()
         characterDao.insertCharacterList(mockDataList)
 
+        //test leading inserted list from db
         val loadFromDB = characterDao.getCharacterList()
         assertThat(loadFromDB.toString(), `is`(mockDataList.toString()))
 
-        val mockData = mock(LotRCharacter::class.java)
+        //test if db contains right data
+        val mockData = LotRCharacter("1", "h", "r", "g", "b", "s", "d", "r", "h", "n", "w")
         assertThat(loadFromDB[0].toString(), `is`(mockData.toString()))
+
+        //test getting character by id from db
+        val characterFromDB = characterDao.getCharacter("1")
+        assertThat(characterFromDB.toString(), `is`(mockData.toString()))
+
+        //test updating character
+        val mockData2 = LotRCharacter("1", "mh", "mr", "mg", "mb", "ms", "md", "mr", "mh", "mn", "mw")
+        characterDao.updateCharacterList(mockData2)
+        val characterFromDB2 = characterDao.getCharacter("1")
+        assertThat(characterFromDB2.toString(), `is`(mockData2.toString()))
+
     }
 
 }
--- app/src/test/java/hu/bme/aut/android/lotrappandroid/persistence/MockTestUtil.kt
+++ app/src/test/java/hu/bme/aut/android/lotrappandroid/persistence/MockTestUtil.kt
@@ -5,4 +5,13 @@ import org.mockito.Mockito.mock
 
 object MockTestUtil {
     fun mockCharacterList() = listOf(mock(LotRCharacter::class.java))
+
+    fun testCharacterList() : List<LotRCharacter>{
+        val list = mutableListOf<LotRCharacter>()
+        list.add(LotRCharacter("1", "h", "r", "g", "b", "s", "d", "r", "h", "n", "w"))
+        list.add(LotRCharacter("2", "h", "r", "g", "b", "s", "d", "r", "h", "n", "w"))
+        list.add(LotRCharacter("3", "h", "r", "g", "b", "s", "d", "r", "h", "n", "w"))
+        list.add(LotRCharacter("4", "h", "r", "g", "b", "s", "d", "r", "h", "n", "w"))
+        return list
+    }
 }
